72ad4a49593e44979c40c9b171fc06efe28ab249
Thunder33345 <Thunder33345@users.noreply.github.com>
2017-04-13 21:40:27 +0800

    Added FactionsPE support

--- src/Thunder33345/PureChatExtension/Loader.php
+++ src/Thunder33345/PureChatExtension/Loader.php
@@ -3,9 +3,11 @@
 namespace Thunder33345\PureChatExtension;
 
 use _64FF00\PureChat\PureChat;
+use factions\FactionsPE;
 use onebone\economyapi\EconomyAPI;
 use pocketmine\plugin\PluginBase;
 use Thunder33345\PureChatExtension\Tags\EconomyAPITag;
+use Thunder33345\PureChatExtension\Tags\Factions\FactionsPETag;
 use Thunder33345\PureChatExtension\Tags\Factions\GenericFactionTag;
 
 class Loader extends PluginBase
@@ -20,8 +22,8 @@ class Loader extends PluginBase
   {
     /** @var PureChat $pc */
     $pc = $this->getServer()->getPluginManager()->getPlugin("PureChat");
-    if ($pc !== null) {
-      if (!method_exists($pc, "registerCustomTag") OR !method_exists($pc, "applyCustomTags")) {
+    if($pc !== null) {
+      if(!method_exists($pc,"registerCustomTag") OR !method_exists($pc,"applyCustomTags")) {
         $this->getLogger()->alert("Incorrect/Incompatible Version Detected");
         $this->getPluginLoader()->disablePlugin($this);
       }
@@ -30,45 +32,60 @@ class Loader extends PluginBase
       $this->getPluginLoader()->disablePlugin($this);
     }
     $tags = $this->getConfig()->get('tags');
-    foreach ($tags as $tag) {
+    foreach($tags as $tag){
       $tag = strtolower($tag);
-      switch ($tag) {
+      switch($tag){
         case "economyapi":
-          if (EconomyAPI::getInstance() !== null) {
-            if (!$pc->registerCustomTag(new EconomyAPITag(), true, $detail))
-              $this->getLogger()->notice("Failed to add EconomyAPI tag\n" . print_r($detail . true) . "\n");
+          if(EconomyAPI::getInstance() !== null) {
+            if(!$pc->registerCustomTag(new EconomyAPITag(),true,$detail))
+              $this->getLogger()->notice("Failed to add EconomyAPI tag\n".print_r($detail.true)."\n");
             else $this->getLogger()->notice("Successfully registered EconomyAPI tag");
           } else $this->getLogger()->notice("Failed to load EconomyAPI!\n");
           break;
         case "faction":
           $config = $this->getConfig()->get("faction");
           $fac = $this->getServer()->getPluginManager()->getPlugin($config['plugin_name']);
-          if ($fac === null) {
+          if($fac === null) {
             $this->getLogger()->alert("Abort loading faction support... Cant find faction plugin, maybe try checking the name?");
             break;
           }
           $functions = [
-            "name" => ["getPlayerFaction"],
-            "rank" => ["isLeader", "isOfficer", "isMember",],
-            "power" => ["getFactionPower"]
+           "name" => ["getPlayerFaction"],
+           "rank" => ["isLeader","isOfficer","isMember",],
+           "power" => ["getFactionPower"],
           ];
           $enable = [];
-          foreach ($functions as $subTag => $subFunctions) {
+          foreach($functions as $subTag => $subFunctions){
             $disable = false;
             $requiredFunctions = "";
-            foreach ($subFunctions as $subFunction) {
-              if (!is_callable([$fac, $subFunction])) {
+            foreach($subFunctions as $subFunction){
+              if(!is_callable([$fac,$subFunction])) {
                 $disable = true;
                 $requiredFunctions .= "$subFunction ";
               }
             }
-            if ($disable) {
+            if($disable) {
               $this->getLogger()->notice("Faction: \"$subTag\" is not supported!(\"$requiredFunctions\" missing)");
               continue;
             }
             $enable[] = $subTag;
           }
-          $this->getPureChat()->registerCustomTag(new GenericFactionTag($this->getServer(), $config, $enable));
+          $this->getPureChat()->registerCustomTag(new GenericFactionTag($this->getServer(),$config,$enable));
+          break;
+        case "factionpe":
+        case "facpe":
+          try{
+            if(FactionsPE::get() === null) {
+              $this->getLogger()->notice("Failed to load FactionPE!");
+            } else {
+              if(!$pc->registerCustomTag(new FactionsPETag($this->getConfig()->get("factionspe")),true,$detail))
+                $this->getLogger()->notice("Failed to add EconomyAPI tag\n".print_r($detail.true)."\n");
+              else $this->getLogger()->notice("Successfully registered EconomyAPI tag");
+            }
+          } catch(\Exception$exception){
+            $this->getLogger()->notice("Failed to load FactionPE!, Debug exception incoming!");
+            $this->getLogger()->logException($exception);
+          }
           break;
         default:
           $this->getLogger()->notice("Tag \"$tag\" does not exist");
@@ -85,6 +102,6 @@ class Loader extends PluginBase
   private function getPureChat()
   {
     $pc = $this->getServer()->getPluginManager()->getPlugin("PureChat");
-    if ($pc instanceof PureChat) return $pc; else return null;
+    if($pc instanceof PureChat) return $pc; else return null;
   }
 }
---
+++ src/Thunder33345/PureChatExtension/Tags/Factions/FactionsPETag.php
@@ -0,0 +1,136 @@
+<?php
+/* Made By Thunder33345 */
+namespace Thunder33345\PureChatExtension\Tags\Factions;
+
+use _64FF00\PureChat\Tags\CustomTagInterface;
+use factions\entity\OfflineMember;
+use factions\manager\Factions;
+use factions\manager\Members;
+use factions\relation\Relation;
+use pocketmine\Player;
+
+class FactionsPETag implements CustomTagInterface,FactionInterface
+{
+  private $config;
+
+  public function __construct($config)
+  {
+    $this->config = $config;
+  }
+
+  public function onAdd() { }
+
+  public function onRemove($code = 0) { }
+
+  public function onError($code) { }
+
+  public function getPrefix(): string { return "factionpe"; }
+
+  public function getAllTags(): array
+  {
+    $ret = [
+     "fname" => "getFactionName",
+     "rank" => "getPlayerFactionRank",
+     "ppower" => "getPlayerPower",
+     "ppowerl" => "getPlayerPowerL",//*shrugs*
+     "fpower" => "getFactionPower",
+     "fpowerl" => "getFactionPowerL",//*shrugs*
+     "fbank" => "getFactionBank",
+    ];
+    return $ret;
+  }
+
+  public function getFactionName(Player $player)
+  {
+    $member = Members::get($player);
+    if(!$member instanceof OfflineMember) {
+      return $this->config['name']['none'];
+    }
+    if(($fac = Factions::getById($member->getFactionId())) === null) {
+      return $this->config['name']['none'];
+    }//no fac
+    return str_replace("%name%",$fac->getName(),$this->config['name']['exist']);
+  }
+
+  public function getPlayerFactionRank(Player $player)
+  {
+    $member = Members::get($player);
+    if(!$member instanceof OfflineMember) {
+      return $this->config['rank']['none'];
+    }
+    if(($fac = Factions::getById($member->getFactionId())) === null) {
+      return $this->config['rank']['none'];
+    }//no fac
+    switch($fac->getRole($member)){
+      case Relation::RECRUIT;
+        return $this->config['rank']['recruit'];
+      case Relation::MEMBER;
+        return $this->config['rank']['member'];
+      case Relation::OFFICER;
+        return $this->config['rank']['officer'];
+      case Relation::LEADER;
+        return $this->config['rank']['leader'];
+      default:
+        return $this->config['rank']['none'];
+    }
+  }
+
+  public function getPlayerPower(Player $player)
+  {
+    $member = Members::get($player);
+    if(!$member instanceof OfflineMember) {
+      return $this->config['power']['none'];
+    }
+    $pow = $member->getPower(false);
+    return str_replace("%power%",$pow,$this->config['power']['power']);
+  }
+
+  public function getPlayerPowerL(Player $player)
+  {
+    $member = Members::get($player);
+    if(!$member instanceof OfflineMember) {
+      return $this->config['power']['none'];
+    }
+    $pow = $member->getPower(false);
+    return str_replace("%power%",$pow,$this->config['power']['power']);
+  }
+
+  public function getFactionPower(Player $player)
+  {
+    $member = Members::get($player);
+    if(!$member instanceof OfflineMember) {
+      return $this->config['power']['none'];
+    }
+    if(($fac = Factions::getById($member->getFactionId())) === null) {
+      return $this->config['power']['none'];
+    }
+    $pow = $fac->getPower(false);
+    return str_replace("%power%",$pow,$this->config['power']['power']);
+  }
+
+  public function getFactionPowerL(Player $player)
+  {
+    $member = Members::get($player);
+    if(!$member instanceof OfflineMember) {
+      return $this->config['power']['none'];
+    }
+    if(($fac = Factions::getById($member->getFactionId())) === null) {
+      return $this->config['power']['none'];
+    }
+    $pow = $fac->getPower(true);
+    return str_replace("%power%",$pow,$this->config['power']['power']);
+  }
+
+  public function getFactionBank(Player $player)
+  {
+    $member = Members::get($player);
+    if(!$member instanceof OfflineMember) {
+      return $this->config['bank']['none'];
+    }
+    if(($fac = Factions::getById($member->getFactionId())) === null) {
+      return $this->config['bank']['none'];
+    }
+    $bank = $fac->getBank();
+    return str_replace("%bank%",$bank,$this->config['bank']['bank']);
+  }
+}
